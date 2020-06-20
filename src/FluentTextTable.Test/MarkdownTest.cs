20d875dafbce74a44868f4dded3ba0b10f5dc406
Atsushi Nakamura <nuits.jp@live.jp>
2020-06-21 04:41:29 +0900

    Removing duplicate test cases from MarkdownTest.cs

--- src/FluentTextTable.Test/MarkdownTest.cs
+++ src/FluentTextTable.Test/MarkdownTest.cs
@@ -64,37 +64,6 @@ namespace FluentTextTable.Test
             }
 
             [Fact]
-            public void WhenSpecifyPadding()
-            {
-
-                var table = TextTable.BuildMarkdown<User>(config =>
-                {
-                    config.HasPadding(2);
-                    config.AddColumn(x => x.Id)
-                        .HasName("ID")
-                        .AlignHorizontal(HorizontalAlignment.Center);
-                    config.AddColumn(x => x.Name)
-                        .HasName("氏名")
-                        .AlignHorizontal(HorizontalAlignment.Left);
-                    config.AddColumn(x => x.Birthday)
-                        .HasFormat("{0:yyyy/MM/dd}");
-                });
-                var text = table.ToString(new[]
-                {
-                    new User {Id = 1, Name = "ビル ゲイツ", Birthday = DateTime.Parse("1955/10/28")},
-                    new User {Id = 123, Name = "Steven Paul Jobs", Birthday = DateTime.Parse("1955/2/24")}
-                });
-
-                Assert.Equal(
-                    @"
-|  ID   |  氏名              |  Birthday    |
-|:-----:|:-------------------|--------------|
-|   1   |  ビル ゲイツ       |  1955/10/28  |
-|  123  |  Steven Paul Jobs  |  1955/02/24  |
-", $"{Environment.NewLine}{text}");
-            }
-
-            [Fact]
             public void WhenMultipleLines()
             {
 
@@ -135,59 +104,15 @@ namespace FluentTextTable.Test
 ", $"{Environment.NewLine}{text}");
             }
 
-            private class User
-            {
-                public int Id { get; set; }
-                public string Name { get; set; }
-                public DateTime Birthday;
-                public string Parents { get; set; }
-                public string[] Occupations { get; set; }
+             private class User
+             {
+                 public int Id { get; set; }
+                 public string Name { get; set; }
+                 public DateTime Birthday;
+                 public string Parents { get; set; }
+                 public string[] Occupations { get; set; }
 
-            }
-
-            [Fact]
-            public void WithAttribute()
-            {
-
-                var table = TextTable.BuildMarkdown<UserWithAttribute>();
-                var text = table.ToString(new[]
-                {
-                    new UserWithAttribute
-                    {
-                        Id = 1,
-                        Name = "Bill Gates",
-                        Birthday = DateTime.Parse("1955/10/28"),
-                        Parents = $"Bill Gates Sr.{Environment.NewLine}Mary Maxwell Gates",
-                        Occupations = new []{"Software developer", "Investor", "Entrepreneur", "Philanthropist"}
-                    }
-                });
-
-                Assert.Equal(
-                    @"
-| ID | Name       | Birthday   | Parents                                  | Occupations                                                      |
-|---:|------------|------------|------------------------------------------|:----------------------------------------------------------------:|
-|  1 | Bill Gates | 1955/10/28 | - Bill Gates Sr.<br>- Mary Maxwell Gates | Software developer<br>Investor<br>Entrepreneur<br>Philanthropist |
-", $"{Environment.NewLine}{text}");
-            }
-
-            private class UserWithAttribute
-            {
-                [Column(Index = 1, Header = "ID", HorizontalAlignment = HorizontalAlignment.Right)]
-                public int Id { get; set; }
-
-                [Column(Index = 2, VerticalAlignment = VerticalAlignment.Center)]
-                public string Name { get; set; }
-
-                [Column(Index = 3, VerticalAlignment = VerticalAlignment.Bottom, Format = "{0:yyyy/MM/dd}")]
-                public DateTime Birthday;
-
-                [Column(Index = 4, VerticalAlignment = VerticalAlignment.Center, Format = "- {0}")]
-                public string Parents { get; set; }
-
-                [Column(Index = 5, HorizontalAlignment = HorizontalAlignment.Center)]
-                public string[] Occupations { get; set; }
-
-            }
+             }
         }
     }
 }
